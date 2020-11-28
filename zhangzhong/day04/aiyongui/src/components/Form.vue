7cf65726ffca59f84d756bc288d660137f1af6ca
kingMid <987322723@qq.com>
2020-11-28 22:06:49 +0800

    修改“考核”中的bug（输入名字会报跨域错误），修复ToDolist输入非空格显示“输入空格无效”bug

--- zhangzhong/day04/aiyongui/src/components/Form.vue
+++ zhangzhong/day04/aiyongui/src/components/Form.vue
@@ -1,4 +1,9 @@
-
+<!--
+ * @Author: 张仲
+ * @Date: 2020-11-27 19:18:58
+ * @Description: In User Settings Edit
+ * @FilePath: \zhangzhong\TodoList.html
+-->
 <template>
   <div id="from">
     <div class="nav">
@@ -26,7 +31,7 @@
     <div class="conent">
       <div class="head">
         <div class="left">
-          <el-button> 返回 </el-button>
+          <el-button> 返回</el-button>
         </div>
         <div class="right">
           <el-button>结束任务</el-button>
@@ -44,9 +49,9 @@
           <div class="li">
             指定筑创星数：{{ this.obj.task_risk_control_amount_small }}
           </div>
-          <div class="li">已报名数{{ this.obj.current_number }}</div>
+          <div class="li">已报名数:{{ this.obj.current_number }}</div>
           <div class="li">
-            任务佣金（单位）{{ this.obj.risk_control_amount }}
+            任务佣金（单位）:{{ this.obj.risk_control_amount }}
           </div>
         </div>
       </div>
@@ -54,33 +59,30 @@
       <p class="p">发布时间:{{ this.obj.audit_time }}</p>
       <div class="lood">
         <p class="p">任务说明：</p>
-        <div class="box">
-          {{ this.obj.position_instructions }}
-        </div>
+        <textarea class="box" v-model=" obj.position_instructions"></textarea>
       </div>
       <el-container class="list">
         <el-header class="Header">
           <h1 class="title">报名详情</h1>
           <div class="btn3">
             <div class="first">
-              <el-button>全部</el-button>
-              <el-button>已报名</el-button>
-              <el-button>待报名</el-button>
+              <el-button class="min">全部</el-button>
+              <el-button class="min">已报名</el-button>
+              <el-button class="min">待报名</el-button>
             </div>
             <div class="sce">
               <el-button>已移除</el-button>
             </div>
-
             <div class="input">
               <el-input
-                class="input-R"
-                placeholder="请输入筑创星/注册手机号"
-                v-model="Usertel"
+                  class="input-R"
+                  placeholder="请输入筑创星/注册手机号"
+                  v-model="Usertel"
               >
                 <el-button
-                  slot="append"
-                  icon="el-icon-search"
-                  @click="scech(Usertel)"
+                    slot="append"
+                    icon="el-icon-search"
+                    @click="scech(Usertel)"
                 ></el-button>
               </el-input>
             </div>
@@ -89,21 +91,34 @@
         <el-main class="Main">
           <div class="table">
             <el-table
-              :data="tableData"
-              stripe
-              style="width: 100%"
-              class="table"
+                :data="tableData"
+                stripe
+                style="width: 100%"
+                class="table"
             >
               <el-table-column prop="sname" label="筑星号"></el-table-column>
-              <el-table-column prop="name" label="姓名"> </el-table-column>
-              <el-table-column prop="tel" label="手机号"> </el-table-column>
+              <el-table-column prop="name" label="姓名"></el-table-column>
+              <el-table-column prop="tel" label="手机号"></el-table-column>
               <el-table-column prop="numsate" label="账号状态">
               </el-table-column>
-              <el-table-column prop="sate" label="报名状态"> </el-table-column>
-              <el-table-column prop="time" label="报名时间"> </el-table-column>
+              <el-table-column prop="sate" label="报名状态"></el-table-column>
+              <el-table-column prop="time" label="报名时间"></el-table-column>
             </el-table>
           </div>
         </el-main>
+        <div class="Listfoorer">
+          <div class="foorRight">
+            <div class="block">
+              <el-pagination
+                  :page-sizes="[100, 200, 300, 400]"
+                  :page-size="100"
+                  layout="total, sizes, prev, pager, next, jumper"
+                  :total="this.statep">
+              </el-pagination>
+            </div>
+          </div>
+
+        </div>
       </el-container>
     </div>
   </div>
@@ -111,10 +126,11 @@
 
 <script>
 export default {
-  data() {
+  data(){
     return {
       Usertel: "",
       obj: {},
+      statep:0,
       tableData: [
         {
           sname: "",
@@ -128,57 +144,84 @@ export default {
     };
   },
   methods: {
-    scech(value) {
-        var opt = "";
-      if (typeof (value * 1) == "number") {
-         opt =
-          "social_credit_code=91410100317580074M&id=1365&page=1&page_number=10&tel=" +
-          value;
-      } else {
-         opt =
-          "social_credit_code=91410100317580074M&id=1365&page=1&page_number=10&=username" +
-          value;
-      }
-      const url =
-        "http://prebin.zhushang.net/zhu_pro_zb/public/index.php/api/fetchBUserTaskEmplyeeAction ";
-
-      fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: opt,
+    fiffer(arr,res){
+      this.statep =0
+      let newArr = arr.map((item) => {
+        item.sname = res.data[0].id_card_number;
+        item.name = res.data[0].username;
+        item.numsate = res.data[0].business_license;
+        item.tel = res.data[0].tel;
+        item.sate = res.data[0].certification_status;
+        item.time = res.data[0].apply_check_time;
+        return item
       })
-        .then((response) => response.json())
-        .then((res) => {
-          this.tableData[0].sname = res.data[0].id_card_number;
-          this.tableData[0].name = res.data[0].username;
-          this.tableData[0].numsate = res.data[0].business_license;
-          this.tableData[0].tel = res.data[0].tel;
-          this.tableData[0].sate = res.data[0].certification_status;
-          this.tableData[0].time = res.data[0].apply_check_time;
-          console.log(this.tableData);
-        });
+      this.statep =1
+      return newArr
+
+    },
+
+    scech(value){
+      this.tableData = [
+        {
+          sname: "",
+          name: "",
+          tel: "",
+          numsate: "",
+          sate: "",
+          time: "",
+        }
+      ];
+      var opt = "";
+      let newVal = parseInt(value)
+      console.log(newVal)
+      if (value == ""){
+        alert("请输入数据再查询")
+
+      } else{
+        if (isNaN(newVal)){
+          opt =
+              "social_credit_code=91410100317580074M&id=1365&page=1&page_number=10&username="+
+              value;
+        } else {
+          opt =
+              "social_credit_code=91410100317580074M&id=1365&page=1&page_number=10&tel="+
+              value;
+        }
+        const url =
+            "http://prebin.zhushang.net/zhu_pro_zb/public/index.php/api/fetchBUserTaskEmplyeeAction ";
+
+        fetch(url,{
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body: opt,
+        })
+            .then((response) => response.json())
+            .then((res) => {
+              this.fiffer(this.tableData,res)
+            });
+      }
     },
   },
-  created() {
+  created(){
     const opts =
-      "social_credit_code=91410100317580074M&id=1365&page=1&page_number=10&taskStatus=all";
+        "social_credit_code=91410100317580074M&id=1365&page=1&page_number=10&taskStatus=all";
     const url =
-      "http://prebin.zhushang.net/zhu_pro_zb/public/index.php/api/fetchAllBUserPostedTaskAction";
+        "http://prebin.zhushang.net/zhu_pro_zb/public/index.php/api/fetchAllBUserPostedTaskAction";
 
-    fetch(url, {
+    fetch(url,{
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
       },
       body: opts,
     })
-      .then((response) => response.json())
-      .then((res) => {
-        console.log(res.data[0]);
-        this.obj = res.data[0];
-      });
+        .then((response) => response.json())
+        .then((res) => {
+          console.log(res.data[0]);
+          this.obj = res.data[0];
+        });
   },
 };
 </script>
@@ -187,50 +230,60 @@ export default {
 .p {
   font-weight: 600;
 }
+
 #from {
   height: 100%;
   float: left;
   background-color: #eee;
   flex: 1;
+
   .nav {
     width: 100%;
     height: 64px;
     background-color: #fff;
+
     .leftLogo {
       float: left;
       width: 68px;
       height: 68px;
       text-align: center;
       line-height: 68px;
+
       .el-icon-s-fold {
         font-size: 30px;
       }
     }
+
     .rightLogo {
       float: right;
       display: flex;
       align-items: center;
       width: 160px;
       height: 68px;
+
       .el-icon-bell {
         flex: 0;
         margin-right: 20px;
         font-size: 20px;
       }
+
       .user {
         flex: 1;
         display: flex;
         align-items: center;
       }
+
       .el-icon-picture-outline-round {
         flex: 1;
         font-size: 30px;
       }
+
       p {
         flex: 1;
       }
     }
   }
+
   .Breadcrumb {
     padding: 10px 65px;
     box-sizing: border-box;
@@ -239,12 +292,14 @@ export default {
     background-color: #fff;
     position: relative;
     border: 1px solid #eee;
+
     .navLi {
       position: absolute;
       bottom: 30px;
       left: 65px;
     }
   }
+
   .conent {
     padding: 10px 20px;
     width: 95%;
@@ -252,23 +307,29 @@ export default {
     margin: 20px auto;
     background-color: #fff;
     box-sizing: border-box;
+
     .head {
       overflow: hidden;
       width: 100%;
+
       .left {
         float: left;
       }
+
       .right {
         float: right;
       }
     }
+
     .rask {
       margin: 20px 0;
       width: 100%;
+
       .ul {
         margin-top: 10px;
         width: 100%;
         height: 70px;
+
         .li {
           float: left;
           width: 30%;
@@ -279,40 +340,60 @@ export default {
         }
       }
     }
+
     .lood {
       width: 100%;
       height: 125px;
       border-bottom: 1px solid #eee;
       padding: 20px 0;
       margin-bottom: 20px;
+
       p {
         float: left;
       }
+
       .box {
         width: 80%;
         height: 100px;
         float: left;
         border: 2px solid #eee;
+        font-size: 18px;
       }
     }
+
     .list {
       .Header {
         display: flex;
+
         .title {
           flex: 1;
         }
+
         .btn3 {
           flex: 2;
           display: flex;
+
           .first {
             flex: 1.5;
+            display: flex;
+            justify-content: flex-start;
+            margin-right: 30px;
+
+            .min {
+              flex: 1;
+              width: 84px;
+              height: 40px;
+            }
           }
+
           .sce {
             flex: 0;
           }
+
           .input {
             flex: 3;
             margin-left: 20px;
+
             .input-R {
               width: 90%;
               float: left;
@@ -320,10 +401,17 @@ export default {
           }
         }
       }
+
       .Main {
         .table {
           text-align: center;
         }
+      }
+      .Listfoorer{
+        text-align: right;
+        padding: 10px 50px;
+        box-sizing: border-box;
+        float: right;
       }
     }
   }

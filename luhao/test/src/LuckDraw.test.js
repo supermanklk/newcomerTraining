4b9005e1d0113b614442f8d6ae69d27c871a3aa7
luhaoa233 <544708790@qq.com>
2020-12-02 10:53:15 +0800

    12.1抽奖

--- luhao/test/src/LuckDraw.test.js
+++ luhao/test/src/LuckDraw.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './LuckDraw';
+import LuckDraw from "./LuckDraw";
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<LuckDraw />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });

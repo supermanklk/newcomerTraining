e6276d7ba14ff7d6c2cba17d3691bf4629890991
kingMid <987322723@qq.com>
2020-12-11 11:48:36 +0800

    规范，注释

--- zhangzhong/php/phpcrud/app.php
+++ zhangzhong/php/phpcrud/app.php
@@ -1,9 +1,10 @@
 <?php
    //设计数据库
 
+//连接数据库
 $conn = new mysqli("localhost","root","root","phpcrud");
 if($conn->connect_error){
-    die("dsdasdasd");
+    die("数据库连接失败");
 }
 
 //构建接口
@@ -19,24 +20,23 @@ if(isset($_GET['action'])){
 //php获取数据
 if ( $action  == "read"){
     $conn->query("set names utf8");
-    $result = $conn->query("SELECT * FROM `users`");
+    $result = $conn->query("SELECT * FROM `users`"); //查询users表的所有内容
 //    var_dump($result);
     $users = array();
-    while ($row = $result->fetch_assoc()){
-        array_push($users , $row);
+    while ($row = $result->fetch_assoc()){                 //通过$result遍历users表将取出的每一项值赋值给$row
+        array_push($users , $row);                 //把每个$row取到的值push到 $users数组中
     }
     //var_dump($users);
-    $res['users'] = $users;
+    $res['users'] = $users;                               //把$users数组赋值给$res中的users数组
 }
 
 //添加数据
 if ( $action  == "addNum"){
     $num = $_POST['num'];
     $conn->query("set names utf8");
-    $result = $conn->query("INSERT INTO `users` (`num`)
- VALUES ('$num')");
+    $result = $conn->query("INSERT INTO `users` (`num`) VALUES ('$num')");//将通过POST请求接收到的num插入数据库中,用$result接受插入状态
 
-    if($result){
+    if($result){                                              //通过判断$result的值（ture|false）来返回插入成功或失败
         $res['message'] = "User added successful";
 
     } else {
@@ -51,11 +51,11 @@ if ( $action  == "update"){
     $id = $_POST['id'];
     $num = $_POST['num'];
 
-    $conn->query("set names utf8");
+    $conn->query("set names utf8");                    //设置编码
 
-    $result = $conn->query("UPDATE `users`  SET `num` = '$num' WHERE `id` = '$id'");
+    $result = $conn->query("UPDATE `users`  SET `num` = '$num' WHERE `id` = '$id'");//将通过POST请求接收到的num和id,根据id查询数据的位置，再修改num的值,用$result接受插入状态
 
-    if($result){
+    if($result){                                                                   //通过判断$result的值（ture|false）来返回数据更新成功或失败
         $res['message'] = "User upDate successful";
 
     } else {
@@ -71,9 +71,9 @@ if ( $action  == "delete"){
 
 
 
-    $result = $conn->query("DELETE FROM `users`  WHERE `id` = '$id'");
+    $result = $conn->query("DELETE FROM `users`  WHERE `id` = '$id'");//将通过POST请求接收到的id,根据id来删除对应的字段,用$result接受插入状态
 
-    if($result){
+    if($result){                                           //通过判断$result的值（ture|false）来返回删除成功或失败
         $res['message'] = "User DELETE successful";
 
     } else {
@@ -85,10 +85,9 @@ if ( $action  == "delete"){
 
 
 
-$conn ->close();
+$conn ->close();                //关闭数据库
 
 
 header("Content-type:application/json");
-header('Access-Control-Allow-Origin:*');
-echo json_encode($res);
-die();
+header('Access-Control-Allow-Origin:*');       //解决前端跨域问题
+echo json_encode($res);                                //将结果转化成json格式
